6c2e29e6bd6a04da87eaabec607f3fcc2115e0ab
Sohaib Zahid <sohaib642@gmail.com>
2023-08-04 12:00:03 +0200

    created routes and register page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Breakfast from "./pages/Breakfast";
+import Lunch from "./pages/Lunch";
+import Dinner from "./pages/Dinner";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/breakfast" element={ <Breakfast /> } />
+        <Route path="/lunch" element={ <Lunch /> } />
+        <Route path="/dinner" element={ <Dinner /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <Register /> } />
       </Routes>
