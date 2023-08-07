ff4f00c1bb3356015445df75440b18b5c6b40c3c
scartage <scartage_estudios@proton.me>
2023-08-07 20:25:40 +0200

    footer with all the correct link to different pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,23 @@ function Footer() {
   return (
     <>
       <div className="footer">
-        Footer
+      <div className="links">
+        <h2>Quick Links</h2>
+        <a href="/About">About Us</a><br></br>
+        <a href="/TermsAndConditions">Terms & Conditions</a><br></br> 
+      </div>
+      <div className="social-media">
+        <h2>Devs who have worked on this project</h2>
+        <a href="https://www.linkedin.com/in/mirjan-kapxhiu-ba742527b">Mirjam</a><br></br>
+        <a href="https://www.linkedin.com/in/sohaibzahid">Sohaib</a><br></br>
+        <a href="https://www.linkedin.com/in/santiagocartagenah">Santiago</a><br></br>
+        <a href="https://www.linkedin.com/in/santiagocartagenah">Marc</a><br></br>
+      </div>
+      <div className="newsletter">
+        <h2>Subscribe to Our Newsletter</h2>
+        <input type="email" placeholder="Enter your email" />
+        <button>Subscribe</button>
+      </div>
       </div>
     </>
   )
