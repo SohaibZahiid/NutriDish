69ddfb122384ec300607cf5e1f07cb285c3c9a60
scartage <scartage_estudios@proton.me>
2023-08-08 12:56:31 +0200

    login and register fixed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,15 +4,15 @@ import "../styles/Register.css";
 function Register() {
   return (
     <>
-      <div className="register section-x2">
-        <div className="container register-container">
+      <div className="register">
+        <div className="register-container">
+          <img src="/imgs/login.jpg"/>
+          <div className="form-container">
           <form className="register-form">
             <label>Name</label>
             <input type="text" placeholder="name" />
             <label>Username</label>
             <input type="text" placeholder="Username" />
-            <label>Email</label>
-            <input type="text" placeholder="Email" />
             <label>Password</label>
             <input type="password" placeholder="Password" />
             <button className="submit btn">Register</button>
@@ -20,24 +20,6 @@ function Register() {
           <p>
             <Link to="/login">Already have an account? Login here.</Link>
           </p>
-        </div>
-      </div>
-      <div className="register">
-        <div className="register-container">
-          <img src="/imgs/login.jpg" />
-          <div className="form-container">
-            <form className="register-form">
-              <label>Name</label>
-              <input type="text" placeholder="name" />
-              <label>Username</label>
-              <input type="text" placeholder="Username" />
-              <label>Password</label>
-              <input type="password" placeholder="Password" />
-              <button className="submit btn">Register</button>
-            </form>
-            <p>
-              <Link to="/login">Already have an account? Login here.</Link>
-            </p>
           </div>
         </div>
       </div>
