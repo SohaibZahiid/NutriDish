e12fc076759c263de268dd18f8c83e54a95ade4f
scartage <scartage_estudios@proton.me>
2023-08-28 10:02:14 +0200

    trying to center the btn

--- src/pages/Calcula.jsx
+++ src/pages/Calcula.jsx
@@ -6,20 +6,6 @@ const Calcula = () => {
   const data = JSON.parse(localStorage.getItem("calories"));
   return (
     <>
-      <div className="planner-container container">
-                      <div className="container-explanation">
-                          <h2>Calculadora de Gasto Calórico</h2>
-                          <p>
-                              Esta herramienta te permite estimar la cantidad de calorías que tu cuerpo necesita en un día, basándose en factores como tu edad, peso, altura, género y nivel de actividad física.
-                          </p>
-                          <p>
-                              La calculadora te proporciona información sobre tu metabolismo basal, las calorías necesarias para mantener el peso, para adelgazar y para ganar peso.
-                          </p>
-                          <p>
-                              Usa esta información como referencia para tus objetivos nutricionales. Siempre es recomendable consultar con un profesional para recibir asesoramiento personalizado.
-                          </p>
-                      </div>
-                  </div> 
       {data && (
         <div className="calculadora">
           <div>
--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -60,6 +60,20 @@ function Planner() {
     <>
       <div className="planner section-x2">
         <div className="planner-container container">
+        <div className="planner-container container">
+                      <div className="container-explanation">
+                          <h2>Calculadora de Gasto Calórico</h2>
+                          <p>
+                              Esta herramienta te permite estimar la cantidad de calorías que tu cuerpo necesita en un día, basándose en factores como tu edad, peso, altura, género y nivel de actividad física.
+                          </p>
+                          <p>
+                              La calculadora te proporciona información sobre tu metabolismo basal, las calorías necesarias para mantener el peso, para adelgazar y para ganar peso.
+                          </p>
+                          <p>
+                              Usa esta información como referencia para tus objetivos nutricionales. Siempre es recomendable consultar con un profesional para recibir asesoramiento personalizado.
+                          </p>
+                      </div>
+                  </div> 
           {!localStorageData && (
             <button
               className="btn-modal btn"
